76f37ebf92393482011c8ba17148e87441087ac3
AnhAnNek <info.vananne@gmail.com>
2022-08-02 22:52:51 +0700

    #4 - van an create method sumOfDigits

--- input.java
+++ input.java
@@ -5,5 +5,16 @@ class Main {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         System.out.println(n);
+        int sum = sumOfDigits(n);
+        System.out.print(sum);
+    }
+
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
